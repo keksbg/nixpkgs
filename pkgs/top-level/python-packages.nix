30360fa0865a05bad5377bf291adcf35cc971b1c
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-07-15 00:13:44 +0200

    python3Packages.pytest: extract test into passthru
    Necessary To escape infinite recursion with hypothesis.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8273,12 +8273,7 @@ in {
 
   pytesseract = callPackage ../development/python-modules/pytesseract { };
 
-  pytest = callPackage ../development/python-modules/pytest {
-    # hypothesis tests require pytest that causes dependency cycle
-    hypothesis = self.hypothesis.override {
-      doCheck = false;
-    };
-  };
+  pytest = callPackage ../development/python-modules/pytest { };
 
   pytest-aio = callPackage ../development/python-modules/pytest-aio { };
 
